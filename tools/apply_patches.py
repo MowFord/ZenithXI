99a144a312aeafe51f272baa2c17b9d1a4b5ba49
MowFord <131182600+MowFord@users.noreply.github.com>
2025-04-24 15:20:56 -0500

    better dbtool messaging for applying git patches

--- tools/apply_patches.py
+++ tools/apply_patches.py
@@ -3,7 +3,7 @@ import subprocess
 
 # If launching from somewhere that isnt <root>/tools/, figure out where dbtool.py is located, and the server root.
 dbtool_dir_path = os.path.normpath(os.path.realpath(os.path.dirname(__file__)))
-patch_dir = "../modules/catseyexi/cpp-patches"
+patch_dirs = [ "../modules/zenith-public/git-patches", "../modules/zenith-private/git-patches" ]
 
 def from_dbtool_path(path):
     return os.path.normpath(os.path.join(dbtool_dir_path, path))
@@ -14,20 +14,21 @@ def from_server_path(path):
     
 
 def apply_patches():
-    # Iterate through the patch files
-    for filename in os.listdir(patch_dir):
-        if filename.endswith(".patch"):
-            patch_path = os.path.abspath(os.path.join(patch_dir, filename))
-            print(os.path.abspath(patch_path))
-            try:
-                # Use subprocess to run the 'git apply' command
-                subprocess.run(["git", "apply", "-R", patch_path], cwd="../", check=True)
-                subprocess.run(["git", "apply", "-v", patch_path], cwd="../", check=True)
-                print(f"Applied patch: {filename}")
-            except subprocess.CalledProcessError as e:
-                print(f"Error applying patch {filename}: {e}")
-            except Exception as e:
-                print(f"Unknown error applying patch {filename}: {e}")
+    # Iterate through the patch dirs
+    for patch_dir in patch_dirs:
+        # Iterate through the patch files
+        for filename in sorted(os.listdir(patch_dir)):
+            if filename.endswith(".patch"):
+                patch_path = os.path.abspath(os.path.join(patch_dir, filename))
+                try:
+                    # Use subprocess to run the 'git apply' command
+                    # subprocess.run(["git", "apply", "-R", patch_path], cwd="../")
+                    subprocess.run(["git", "apply", "--ignore-whitespace", "-v", patch_path], cwd="../", check=True)
+                    print(f"SUCCESS: Applied patch: {filename}")
+                except subprocess.CalledProcessError as e:
+                    print(f"ERROR: applying patch {filename}: {e}")
+                except Exception as e:
+                    print(f"ERROR: Unknown applying patch {filename}: {e}")
 
 if __name__ == "__main__":
     apply_patches()
