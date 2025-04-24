054a3ead830ad6577c89632d41c506c1e2cc47e4
MowFord <131182600+MowFord@users.noreply.github.com>
2025-04-24 16:14:24 -0500

    simple quest to test LQS

---
+++ modules/zenith-public/lua/1-enum/xi/item.lua
@@ -0,0 +1,11 @@
+-----------------------------------
+-- Additional xi.item entries
+-----------------------------------
+local m = Module:new('enum_item')
+
+-----------------------------------
+-- Quests
+-----------------------------------
+xi.item.SLICE_OF_GRILLED_HARE2 = 4371
+
+return m
