b1413987f30676876ce042f00b246996ccfaee84
MowFord <131182600+MowFord@users.noreply.github.com>
2025-07-12 15:08:34 -0500

    Documentation, remove Lua file.
    Refactor due to style issues
    Add suggested changes from PR review
    
    Co-authored-by: Apogeezer <219874095+apogeezer@users.noreply.github.com>

--- modules/zenith-public/cpp/fishing_fatigue.cpp
+++ modules/zenith-public/cpp/fishing_fatigue.cpp
@@ -1,6 +1,28 @@
-/*---------------------------------
--- Fishing Fatigue
----------------------------------*/
+/************************************************************************
+ * Fishing Fatigue
+ *
+ * This module implements fishing fatigue, which limits the number of fish
+ * a player can catch in a day. The regular limit is 200 fish/items as
+ * per retail, but is limited to 20 fish/items for new accounts until
+ * they have reached a required rank in any of the three nations.
+ *
+ * The amount of fish caught is tracked in the player's char_vars, and
+ * resets at JST midnight. We inspect the Fishing Minigame 2 Packet
+ * (0x110) and check if minigame sends the ANIMATION_FISHING_CAUGHT
+ * animation. If the character had hooked a small/big fish or an item
+ * as well, we increase the char_var by 1.
+ *
+ * Limiting the fishing action is done by inspecting the Player Action
+ * packet (0x01A) to see if the player is attempting a Fish Action (0x0E).
+ * If the daily cap was reached, we set the lastCastTime to 600 seconds in
+ * the future. This will cause the FishingAction to automatically fail
+ * into CatchNothing. A message will be sent to the player informing them
+ * the limit has been reached.
+ *
+ * This script will allow for players to resume fishing after Ranking
+ * up their nation and changing zones, up to the new limit. Changing
+ * Allegiance won't cause you to lose your unlocked 200 daily cap.
+ ************************************************************************/
 #include "map/packets/chat_message.h"
 #include "map/utils/charutils.h"
 #include "map/utils/moduleutils.h"
@@ -12,18 +34,15 @@ class FishingFatigueModule : public CPPModule
 {
     std::string CAUGHT = "[FISHING]CAUGHT";
 
-    const uint8  SANDORIA_NATION_ID = 0;
-    const uint8  BASTOK_NATION_ID   = 1;
-    const uint8  WINDURST_NATION_ID = 2;
-    const uint8  RANK_REQUIRED      = 3;
-    const uint16 FISHING_LOWER      = 20;  // Lower limit for fishing
-    const uint16 FISHING_UPPER      = 200; // Upper limit for fishing
+    const uint8  RANK_REQUIRED = 3;
+    const uint16 FISHING_LOWER = 20;  // Lower limit for fishing
+    const uint16 FISHING_UPPER = 200; // Upper limit for fishing
 
     uint16 getCaughtLimit(CCharEntity* PChar)
     {
-        uint8 rankSandoria           = PChar->profile.rank[SANDORIA_NATION_ID];
-        uint8 rankBastok             = PChar->profile.rank[BASTOK_NATION_ID];
-        uint8 rankWindurst           = PChar->profile.rank[WINDURST_NATION_ID];
+        uint8 rankSandoria           = PChar->profile.rank[NATION_SANDORIA];
+        uint8 rankBastok             = PChar->profile.rank[NATION_BASTOK];
+        uint8 rankWindurst           = PChar->profile.rank[NATION_WINDURST];
         bool  isSufficientNationRank = (rankSandoria >= RANK_REQUIRED || rankBastok >= RANK_REQUIRED || rankWindurst >= RANK_REQUIRED);
 
         return isSufficientNationRank ? FISHING_UPPER : FISHING_LOWER;
@@ -36,9 +55,7 @@ class FishingFatigueModule : public CPPModule
          *                        PacketParser methods
          ************************************************************************/
         {
-            auto playerAction    = PacketParser[0x01A];
-            auto fishingMinigame = PacketParser[0x110];
-
+            auto playerAction        = PacketParser[0x01A];
             auto playerActionFatigue = [this, playerAction](MapSession* const PSession, CCharEntity* const PChar, CBasicPacket& data) -> void
             {
                 TracyZoneScoped;
@@ -63,6 +80,7 @@ class FishingFatigueModule : public CPPModule
             };
             PacketParser[0x01A] = playerActionFatigue;
 
+            auto fishingMinigame       = PacketParser[0x110];
             auto fishingMinigameResult = [this, fishingMinigame](MapSession* const PSession, CCharEntity* const PChar, CBasicPacket& data) -> void
             {
                 TracyZoneScoped;
--- modules/zenith-public/lua/4-additive/fishing_fatigue.lua
+++
@@ -1,8 +0,0 @@
------------------------------------
--- Fishing Fatigue
------------------------------------
-require("modules/module_utils")
------------------------------------
-local m = Module:new("fishing_fatigue")
-
-return m
