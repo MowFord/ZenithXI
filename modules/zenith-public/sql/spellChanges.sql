55faa3b594b6062176e1b8268b5a273d2603b5c9
NathanielCFisher <76634100+NathanielCFisher@users.noreply.github.com>
2025-11-04 13:58:11 +0100

    refactor whitespace

--- modules/zenith-public/sql/spellChanges.sql
+++ modules/zenith-public/sql/spellChanges.sql
@@ -10,14 +10,14 @@
 -- Absorb spells' cast time reverted to pre-August 12, 2014 update values
 
 UPDATE `spell_list` SET `castTime` = 2000 WHERE `name` IN (
-	'absorb-acc',
-	'absorb-attri',
-	'absorb-str',
-	'absorb-dex',
-	'absorb-vit',
-	'absorb-agi',
-	'absorb-int',
-	'absorb-mnd',
-	'absorb-chr',
-	'absorb-tp'
+    'absorb-acc',
+    'absorb-attri',
+    'absorb-str',
+    'absorb-dex',
+    'absorb-vit',
+    'absorb-agi',
+    'absorb-int',
+    'absorb-mnd',
+    'absorb-chr',
+    'absorb-tp'
 );
